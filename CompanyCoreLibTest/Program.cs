561390e7e1c952ef43c7ff778e5c1faf5eb03581
angularstoneru <87306719+angularstoneru@users.noreply.github.com>
2024-12-25 18:38:25 +0300

    Initial commit

---
+++ CompanyCoreLibTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using CompanyCoreLib;
+
+namespace CompanyCoreLibTest
+{
+    internal class Program
+    {
+
+        //public static List<string> testdatesstrings = new List<string>();
+		public static List<DateTime> testdates = new List<DateTime>();
+
+		static void Main(string[] args)
+        {
+			string[] testdatesstrings = File.ReadLines( "testdates.txt" ).ToArray();
+
+			foreach( string datestring in testdatesstrings )
+			{
+				if( datestring.StartsWith( "#" ) )
+					continue;
+				testdates.Add( DateTime.Parse( datestring ) );
+			}
+
+			Analytics analytics = new Analytics();
+			List<DateTime> dates = analytics.PopularMonths( testdates );
+			Console.WriteLine( "Returned {0:d} dates.", dates.Count );
+
+			Console.WriteLine( "Source:" );
+			foreach( DateTime date in testdates )
+			{
+				Console.WriteLine( date.ToString( "yyyy MM dd HH:mm:ss" ) );
+			}
+			Console.WriteLine( "\nResult:" );
+			foreach( DateTime date in dates )
+			{
+				Console.WriteLine( date.ToString( "yyyy-MM-dd HH:mm" ) );
+			}
+		}
+	}
+}
